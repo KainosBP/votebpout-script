762a0d416e1d9bf8d1edd42095b0362d6983e31a
jbuice <jbuice@kainostech.com>
2018-11-29 22:52:34 -0600

    Create teclos.sh

---
+++ teclos.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Scrip Created by J.T. Buice | kainosbp.com
+#
+
+PATH="/telos/stage3.0/build/programs"
+
+WALLETHOST="127.0.0.1"
+APIHOST="127.0.0.1"
+APIPORT="8888"
+WALLETPORT="8999"
+
+#$PATH/teclos/teclos -u http://$NODEHOST:$NODEPORT --wallet-url http://$WALLETHOST:$WALLETPORT "$@"
+$PATH/teclos/teclos -u http://$APIHOST:$APIPORT "$@"
